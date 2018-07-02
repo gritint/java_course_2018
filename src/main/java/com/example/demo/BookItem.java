734a4496f35e5ae9f064149f40ed98913ae1c37f
Gritin Tuamsomboon <gritin.t@kbtg.tech>
2018-07-02 12:31:59 +0700

    Update book

--- src/main/java/com/example/demo/BookItem.java
+++ src/main/java/com/example/demo/BookItem.java
@@ -1,19 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.Book;
+
 public class BookItem {
+    
     private Book book;
     private int quantity;
 
     public BookItem(Book book, int quantity) {
         this.book = book;
         this.quantity = quantity;
-        
+
     }
-    public Book getBook() {
-                return book;
+
+//    public Book getBook() {
+//        return book;
+//    }
+//
+//    public void add(BookItem bookItem) {
+//        // TODO Auto-generated method stub
+//
+//    }
+
+    public double getTotalPrice() {
+        return book.getPrice() * quantity;
     }
-    
-    
-    
 
 }
--- src/main/java/com/example/demo/Order.java
+++ src/main/java/com/example/demo/Order.java
@@ -1,15 +1,18 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
 
-    private BookItem bookItem;
-    private int bookAmount;
+//    private BookItem bookItem;
+//    private int bookAmount;
     private double totalPrice;
-    private int discount;
+    private double discount;
     private double netPrice;
 
-    public int getBookAmountl() {
-        return this.bookAmount;
+    public int getBookAmount() {
+        return this.bookItem.size();
     }
 
     public double getTotalPrice() {
@@ -23,16 +26,21 @@ public class Order {
     public double getNetlPrice() {
         return this.netPrice;
     }
+    
+    private List<BookItem> bookItem = new ArrayList<>();
+    public void addItem(BookItem bookItem) {
+        this.bookItem.add(bookItem);
+    }
 
     public void process() {
         if (this.bookItem != null) {
-            this.bookAmount = 1;
+            calculateTotalPrice();
 
-            // calculatePrice
-            this.totalPrice = bookItem.getBook().getPrice();
+//            // calculatePrice
+//            this.totalPrice = bookItem.getBook().getPrice();
 
             // Calculate discount
-            this.discount = 0;
+            calculateDiscount();
 
             // Calculate net price
             this.netPrice = getTotalPrice() - getDiscount();
@@ -40,9 +48,24 @@ public class Order {
         }
     }
 
-    public void addItem(BookItem bookItem) {
-        this.bookItem = bookItem;
-
+    private void calculateDiscount() {
+        this.discount = 0;
+        if(getBookAmount() == 2) {
+            this.discount =  getTotalPrice() * 0.05;
+        }
+        if(getBookAmount() == 3) {
+            this.discount =  getTotalPrice() * 0.1;
+        }
     }
 
+//    public void addItem(BookItem bookItem) {
+//        this.bookItem = bookItem;
+//
+//    }
+    private void calculateTotalPrice() {
+        for (BookItem book : bookItem) {
+            this.totalPrice += book.getTotalPrice();
+
+}
+    }
 }
