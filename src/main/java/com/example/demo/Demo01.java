4db8b5219e75366378b22c333d9fadc488d35d5c
Gritin Tuamsomboon <gritin.t@kbtg.tech>
2018-07-05 10:32:59 +0700

    Update

--- src/main/java/com/example/demo/Demo01.java
+++ src/main/java/com/example/demo/Demo01.java
@@ -9,7 +9,7 @@ import java.util.Set;
 public class Demo01 {
      public static void main(String[]args) {
          Demo01 demo01 =new Demo01();
-//         demo01.demolist();
+       demo01.demolist();
          demo01.demonMap();
          
      }
